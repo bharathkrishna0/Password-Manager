da4c872a962f5076c76ab202f76b5510d610688f
Bharath Krishna <bharathkrishna2006@gmail.com>
2024-08-27 19:28:23 +0530

    Add files via upload

---
+++ oassward manager.py
@@ -0,0 +1,47 @@
+from cryptography.fernet import Fernet
+
+
+
+
+def write_key():
+    key = Fernet.generate_key()
+    with open("key.key","wb") as key_file:
+        key_file.write(key)
+
+def load_key():
+    file = open("key.key","rb")
+    key = file.read()
+    file.close()
+    return key
+
+
+
+
+master_pwd = input("what is the master password ?")
+key = load_key() + master_pwd.encode()
+fer =Fernet(key)
+def view():
+    with open ('passwords.txt ','r') as f :
+        for line in f.readlines():
+            data = line.rstrip()
+            user,passw = data.split("|")
+            print("User",user, "Password",fer.decrypt(pwd.encode()).decode())
+
+def add():
+    name = input('account name:')
+    pwd = input("Password:")
+
+    with open("Passwords", "a") as f :
+        f.write(name +"|"+ str(fer.encrypt(pwd.encode())+ "\n" ))
+
+while True :
+    mode = input("would you like to add a new or view existing ones (view,add),press q to quit")
+    if mode =="q":
+        break
+    if mode == "view":
+        view()
+    elif mode == "add":
+        add()
+    else:
+        print("invalid mode.")
+        continue
